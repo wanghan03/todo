0c6076ea2e1f3b3f2eef7c540083b76c7062fa2f
wanghan03 <Han@13-70-27.client.wireless.msu.edu>
2015-03-28 18:29:10 -0400

    Initial Commit

---
+++ Todo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Todo
+//
+//  Created by Han Wang on 3/28/15.
+//  Copyright (c) 2015 Han Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Todo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Todo
+//
+//  Created by Han Wang on 3/28/15.
+//  Copyright (c) 2015 Han Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
